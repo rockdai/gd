1bfc9fffaee27f98bfd7911462bf00f7cc8f31eb
rock <rockdai321@gmail.com>
2025-12-02 17:19:23 +0800

    fix: config

--- config.js
+++ config.js
@@ -2,24 +2,14 @@
 
 const DOMAIN = exports.DOMAIN = {
   XFYJ: 'xfyj.limengna.com',
-  XFYJ_BK: 'unicom.limengna.com',
 };
 
 exports.RuleConfig = [{
-  product: 'ecs',
-  groupId: 'sg-bp15zc9odnod2fdkyid4',
-  regionId: 'cn-hangzhou',
-  ruleList: [
-    { name: DOMAIN.XFYJ, id: 'sgr-bp1h6rk3pgct6dzmdfiw' },
-    { name: DOMAIN.XFYJ_BK, id: 'sgr-bp1fvr0ojwfsynjdk423' },
-  ],
-}, {
   product: 'swas-open',
   instanceId: '5c7bfc974c694ee498de6bbb7c8e5bab',
   regionId: 'cn-hangzhou',
   ruleList: [
     { name: DOMAIN.XFYJ, id: 'de66f0d108914ffa90cc5ee4b07ae178' },
-    { name: DOMAIN.XFYJ_BK, id: '48dc7007353e490baf79e1104e566dc0' },
   ],
 }, {
   product: 'swas-open',
@@ -27,15 +17,6 @@ exports.RuleConfig = [{
   regionId: 'ap-northeast-1',
   ruleList: [
     { name: DOMAIN.XFYJ, id: 'cc75fecab9b74db0be1548f0f2207b99' },
-    { name: DOMAIN.XFYJ_BK, id: 'c6023c53805b4a4d9feab50aa497dac5' },
-  ],
-}, {
-  product: 'swas-open',
-  instanceId: '8cddeed727534e3fbfcdc426c2eeb5a2',
-  regionId: 'cn-hangzhou',
-  ruleList: [
-    { name: DOMAIN.XFYJ, id: 'c97e325c56df4f2ba3df36553185e79e' },
-    { name: DOMAIN.XFYJ_BK, id: '09dcbccdf474475daa7a7b39fbd64b07' },
   ],
 }, {
   product: 'swas-open',
@@ -43,7 +24,6 @@ exports.RuleConfig = [{
   regionId: 'us-west-1',
   ruleList: [
     { name: DOMAIN.XFYJ, id: '7ea5c24bb3c148c0b67b9719873c6dec' },
-    { name: DOMAIN.XFYJ_BK, id: 'ec66ce20a3af441082fa4cf1323d9d40' },
   ],
 }, {
   // a.keydiary.dev
